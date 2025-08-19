2cf601ed5bd9fba1f01fd98767c5d39b4262a549
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-19 17:52:35 +0200

    Create math.h

---
+++ include/math.h
@@ -0,0 +1,10 @@
+#ifndef SWING_MATH_H
+#define SWING_MATH_H
+
+// Simple math operations for Swing
+int swing_add(int a, int b);
+int swing_sub(int a, int b);
+int swing_mul(int a, int b);
+int swing_div(int a, int b);
+
+#endif
