fccd02551653535675bb8405a8738308601c107b
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-19 15:03:35 +0200

    Create commands.h

---
+++ include/commands.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+    const char *name;
+    bool (*match)(const char *line);
+    void (*emit)(const char *line, FILE *out);
+} Command;
+
+extern Command commands[];
+
+#endif
