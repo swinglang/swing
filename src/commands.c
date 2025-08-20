c4ac51387fa306bd265ff7ac94e98c963111dfa2
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-20 15:17:31 +0200

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -3,7 +3,8 @@
 // forward declarations
 extern Command cmd_print;
 extern Command cmd_let;
-extern Command cmd_ref;
+extern Command cmd_int;
+extern Command cmd_void;
 
 Command *commands[] = {
     &cmd_print,
