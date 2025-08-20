8baf6507f9ce0e3ea25b2f4bacc064141dfa8895
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-20 15:11:30 +0200

    Update commands.c

--- src/commands.c
+++ src/commands.c
@@ -1,14 +1,13 @@
-#include "../include/commands.h"
+#include "commands.h"
 
-// forward declarations from src/cmd/*.c
+// forward declarations
 extern Command cmd_print;
 extern Command cmd_let;
 extern Command cmd_ref;
-// etc...
 
-Command commands[] = {
-    cmd_print,
-    cmd_let,
-    cmd_ref,
-    {NULL, NULL, NULL} // sentinel
+Command *commands[] = {
+    &cmd_print,
+    &cmd_let,
+    &cmd_ref,
+    NULL // sentinel
 };
