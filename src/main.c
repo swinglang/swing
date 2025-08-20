6d8aa0bfb1e5649040a5a43bed49eb6b59b91a9f
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-20 15:23:41 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: swingc <file.wh>\n");
+        fprintf(stderr, "Usage: swingc <file.sw>\n");
         return 1;
     }
 
