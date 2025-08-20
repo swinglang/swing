5a27914bc6330a8ecc69e89d237e6ac311135355
Leon <132808925+Leon8326@users.noreply.github.com>
2025-08-20 15:30:53 +0200

    Create compiler.h

---
+++ include/compiler.h
@@ -0,0 +1,9 @@
+#ifndef COMPILER_H
+#define COMPILER_H
+
+#include <stdio.h>
+
+// Declaration of functions implemented in compiler.c
+void process_line(const char *line, FILE *out);
+
+#endif // COMPILER_H
